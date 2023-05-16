8a0ee7c459ed8ffa6968c282c8f51a9312a6a5a7
Jorge Valenzuela <valenzuela.jorgediaz@gmail.com>
2023-05-16 18:13:30 -0400

    agrega mas pruebas

--- src/calculaprueba/CalculadoraFinanciera.java
+++ src/calculaprueba/CalculadoraFinanciera.java
@@ -9,4 +9,15 @@ public class CalculadoraFinanciera {
         return capital * Math.pow((1 + tasa), periodo);
     }
     
+    public double calcularAmortizacion(double capital, double tasa, int periodo) {
+        double cuota = capital * tasa / (1 - Math.pow((1 + tasa), -periodo));
+        return cuota;
+    }
+    
+    public double calcularTasaEfectiva(double tasaNominal, int frecuencia) {
+        double tasaEfectiva = Math.pow((1 + tasaNominal / frecuencia), frecuencia) - 1;
+        return tasaEfectiva;
+    }
+    
+    // Otros métodos y funcionalidades de la calculadora financiera
 }
--- src/calculaprueba/CalculadoraFinancieraTest.java
+++ src/calculaprueba/CalculadoraFinancieraTest.java
@@ -1,27 +1,37 @@
 package calculaprueba;
-
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class CalculadoraFinancieraTest {
-
-
-	public void testCalcularInteresSimple1() {
+    
+    @Test
+    public void testCalcularInteresSimple() {
         CalculadoraFinanciera calculadoraFinanciera = new CalculadoraFinanciera();
         double resultado = calculadoraFinanciera.calcularInteresSimple(1000, 0.05, 2);
         assertEquals(100.0, resultado, 0.01);
     }
     
     @Test
-    public void testCalcularInteresCompuesto1() {
+    public void testCalcularInteresCompuesto() {
         CalculadoraFinanciera calculadoraFinanciera = new CalculadoraFinanciera();
         double resultado = calculadoraFinanciera.calcularInteresCompuesto(1000, 0.05, 2);
         assertEquals(1102.5, resultado, 0.01);
     }
-	
-	
-	
-	
-
+    
+    @Test
+    public void testCalcularAmortizacion() {
+        CalculadoraFinanciera calculadoraFinanciera = new CalculadoraFinanciera();
+        double resultado = calculadoraFinanciera.calcularAmortizacion(5000, 0.1, 5);
+        assertEquals(1318.987403973726, resultado, 0.01);
+    }
+    
+    @Test
+    public void testCalcularTasaEfectiva() {
+        CalculadoraFinanciera calculadoraFinanciera = new CalculadoraFinanciera();
+        double resultado = calculadoraFinanciera.calcularTasaEfectiva(0.1, 12);
+        assertEquals(0.10471, resultado, 0.00001);
+    }
+    
+    // Agrega más pruebas para otros métodos y casos de la CalculadoraFinanciera
 }
+
